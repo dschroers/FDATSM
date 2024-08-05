e2286d71fba1b3db0c8b393bf57292c9704ca031
dschroers <dennis.schroers@gmx.de>
2024-08-05 13:23:47 +0200

    Estimator Changes

--- R/Truncated_Covariation_Estimator.R
+++ R/Truncated_Covariation_Estimator.R
@@ -65,7 +65,7 @@ Truncated.Covariation.estimator <- function(x,# discount curve data x[i,j]=p_{i\
       loads<-numeric(ncol(Truncated.variation))
       EG2<-eigen(Truncated.variation)
       for (i in 1:ncol(Truncated.variation)) {
-        loads<-sum(EG2$values[1:i])
+        loads[i]<-sum(EG2$values[1:i])
       }
       expl.var<-loads/sum(EG2$values)
       par(mfrow = c(1, 2))
