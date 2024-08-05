55d4cc9db5276f42af7b73d07ebcf9a13138f0bd
dschroers <dennis.schroers@gmx.de>
2024-08-05 13:43:10 +0200

    Chng Est

--- R/Truncated_Covariation_Estimator.R
+++ R/Truncated_Covariation_Estimator.R
@@ -70,7 +70,7 @@ Truncated.Covariation.estimator <- function(x,# discount curve data x[i,j]=p_{i\
       par(mfrow = c(1, 2))
       persp(Truncated.variation,xlab= "Time to maturity (years)")
       plot(expl.var[1:10], type = "p")
-      abline(h = 0, v = 0, col = "gray60")
+      abline(h = .99, col = "gray60")
     }
 
   return(list("IV" = Truncated.variation, "locs" = locs, "C.Prel" =C.Prel, "adj.increments" = adjusted.increments))
