f5b20d9e6e167db2df93df5ee5a1473fda4566d6
dschroers <dennis.schroers@gmx.de>
2024-08-05 15:10:18 +0200

    est chngs

--- R/Truncated_Covariation_Estimator.R
+++ R/Truncated_Covariation_Estimator.R
@@ -81,9 +81,9 @@ Truncated.Covariation.estimator <- function(x,# discount curve data x[i,j]=p_{i\
       persp(Truncated.variation,xlab= "Time to maturity (years)")
       plot(expl.var[1:10], type = "p", ylab = " Explained Variation", xlab = "Nr. of eigenvalues")
       abline(h = .99, col = "gray60")
-      plot(norms, type = "l", ylab = "L2 norms of price curves", xlab = "time")
+      plot(norms, type = "l", ylab = "L2 norms of price curves", xlab = "time", x= seq(from = 0, to = 1, length.out=100))
       points(x=locs, y= norms[locs], col = "darkgreen")
-      plot(adj.norms, type = "l", ylab = "L2 norms of difference returns", xlab = "time")
+      plot(adj.norms, type = "l", ylab = "L2 norms of difference returns", xlab = "time", x= seq(from = 0, to = 1, length.out=100))
       points(x=locs, y= adj.norms[locs], col = "darkgreen")
     }
 
